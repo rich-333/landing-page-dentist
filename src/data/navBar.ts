ea2b28b0326007b83c59d2e31ac0ba40444bc692
richard <TU_USUARIO@users.noreply.github.com>
2025-09-24 22:14:31 -0400

    Se agrego los iconos e imagenes para el componente Hero ademas de estilos y su estructura
    Esto mejorara la apariencia y disposicion de cada elemento dentro de la pagina.

---
+++ src/data/navBar.ts
@@ -0,0 +1,23 @@
+export interface NavLinks {
+  name: string;
+  href: string;
+}
+
+export const navLinks: NavLinks[] = [
+  {
+    name: "Inicio",
+    href: "#inicio",
+  },
+  {
+    name: "Servicios",
+    href: "#servicio",
+  },
+  {
+    name: "Nosotros",
+    href: "#nosotros",
+  },
+  {
+    name: "Contacto",
+    href: "#contacto",
+  },
+];
