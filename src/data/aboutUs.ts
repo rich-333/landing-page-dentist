204f1b19234ce9b1bcc57d9acfdcc0f443670582
richard <TU_USUARIO@users.noreply.github.com>
2025-10-11 19:25:13 -0400

    Se agregaron los elementos y estilos para crear el componente AboutUs
    Esto mostrara informacion acerca del odontologo para que los usuarios vean esa informacion de forma ordenada.

---
+++ src/data/aboutUs.ts
@@ -0,0 +1,39 @@
+export interface AboutUs {
+  qualities: string;
+}
+
+export const aboutUs: AboutUs[] = [
+  {
+    qualities:
+      "Médica Cirujano, graduada en la Universidad Nacional de Córdoba, Argentina.",
+  },
+  {
+    qualities: "Odontóloga, graduada en la Universidad de París, Francia",
+  },
+  {
+    qualities:
+      "Docente de la Universidad Nacional de Córdoba desde el año 1975",
+  },
+  {
+    qualities: "Miembro de las siguientes Academias:",
+  },
+  {
+    qualities: "A.L.A.O: Academia Latinoamericana de Oseointegración.",
+  },
+  {
+    qualities:
+      "A.I.I.O: Academia Internacional de Implantes y Oseointegración . Miembro Fundador",
+  },
+  {
+    qualities:
+      "A.V.O.I.O: Academia Venezolana de Oseointegración e Implantologìa Oral. Miembro Honorario.",
+  },
+  {
+    qualities:
+      "Pierre Fauchard Academy: Academia de Honor de Odontologìa de E.E.U.U.",
+  },
+  {
+    qualities:
+      "A.I.I.P: Academia Internacional de Implantología y Periodoncia – Barcelona. Presidente Internacional.",
+  },
+];
