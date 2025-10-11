dda7b099101bfb56c20909611b3eb25d5b28b9cd
richard <TU_USUARIO@users.noreply.github.com>
2025-10-11 18:11:37 -0400

    Se agrego la seccion de testimonios para mostrar los comentarios acerca del consultorio
    Esto mostrara tarjetas con cada comentario.

---
+++ src/data/testimonial.ts
@@ -0,0 +1,51 @@
+import star from "../assets/icons/star.svg?url";
+
+export interface Testimony {
+  name: string;
+  comment: string;
+  score: string;
+}
+
+export const testimonies: Testimony[] = [
+  {
+    name: "Maria Paz",
+    comment:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit conubia, massa facilisi lectus ullamcorper ridiculus potenti iaculis nibh, interdum faucibus ad diam vivamus vehicula fames.",
+    score: star,
+  } /*,
+
+  {
+    name: "Maria Paz",
+    comment:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit conubia, massa facilisi lectus ullamcorper ridiculus potenti iaculis nibh, interdum faucibus ad diam vivamus vehicula fames.",
+    score: star,
+  },
+
+  {
+    name: "Maria Paz",
+    comment:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit conubia, massa facilisi lectus ullamcorper ridiculus potenti iaculis nibh, interdum faucibus ad diam vivamus vehicula fames.",
+    score: star,
+  },
+
+  {
+    name: "Maria Paz",
+    comment:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit conubia, massa facilisi lectus ullamcorper ridiculus potenti iaculis nibh, interdum faucibus ad diam vivamus vehicula fames.",
+    score: star,
+  },
+
+  {
+    name: "Maria Paz",
+    comment:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit conubia, massa facilisi lectus ullamcorper ridiculus potenti iaculis nibh, interdum faucibus ad diam vivamus vehicula fames.",
+    score: star,
+  },
+
+  {
+    name: "Maria Paz",
+    comment:
+      "Lorem ipsum dolor sit amet consectetur adipiscing elit conubia, massa facilisi lectus ullamcorper ridiculus potenti iaculis nibh, interdum faucibus ad diam vivamus vehicula fames.",
+    score: star,
+  },*/,
+];
